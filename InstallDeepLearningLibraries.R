6eae29f6b9e94c6175bc9941f021c98cd304f8c3
Jee Tae Chang <taecjee@naver.com>
2019-12-16 14:40:12 +0900

    Install Deep Learning Libraries

---
+++ InstallDeepLearningLibraries.R
@@ -0,0 +1,5 @@
+# Install the MXNet package for Windows
+cran <- getOption("repos")
+cran["dmlc"] <- "https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/R/CRAN"
+options(repos = cran)
+install.packages("mxnet")
